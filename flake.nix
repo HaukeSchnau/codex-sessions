bbc2a61fbc1d80fc68e1df6d74fd107bc4fb2f96
Hauke Schnau <hauke@schnau.dev>
2026-05-26 23:28:13 +0200

    Create flake with mkShellNoCC

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+        ];
+      };
+    });
+}
