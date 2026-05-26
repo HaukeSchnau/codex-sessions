0f08e3253a30622e7c85d1500b901b35a2d59ad0
Hauke Schnau <hauke@schnau.dev>
2026-05-26 23:28:35 +0200

    chore: update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,16 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [
-        ];
-      };
-    });
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+          ];
+        };
+      }
+    );
 }
